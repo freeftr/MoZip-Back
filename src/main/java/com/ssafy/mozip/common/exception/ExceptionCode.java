c0fb566c91d61f3ef560480088366528a402b9f8
freeftr <jonghaparkdevelop@gmail.com>
2024-12-28 22:35:23 +0900

    feat: 공통 예외 코드 정의

--- src/main/java/com/ssafy/mozip/common/exception/ExceptionCode.java
+++ src/main/java/com/ssafy/mozip/common/exception/ExceptionCode.java
@@ -7,7 +7,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExceptionCode {
 
-    INVALID_REQUEST(1000, "유효하지 않은 요청입니다");
+    INVALID_REQUEST(1000, "유효하지 않은 요청입니다."),
+    NOT_FOUND_USER_ID(1002, "요청 ID에 해당하는 유저가 존재하지 않습니다."),
+
+    UNABLE_TO_GET_USER_INFO(2001, "소셜 로그인 공급자로부터 유저 정보를 받아올 수 없습니다."),
+    UNABLE_TO_GET_ACCESS_TOKEN(2002, "소셜 로그인 공급자로부터 인증 토큰을 받아올 수 없습니다."),
+
+    UNAUTHORIZED_ACCESS(3000, "접근할 수 없는 리소스입니다."),
+    INVALID_REFRESH_TOKEN(3001, "유효하지 않은 Refresh Token입니다."),
+    FAILED_TO_VALIDATE_TOKEN(3002, "토큰 검증에 실패했습니다."),
+    INVALID_ACCESS_TOKEN(3003, "유효하지 않은 Access Token입니다.");
 
     private final int code;
     private final String message;
