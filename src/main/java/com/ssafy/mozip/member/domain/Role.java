128d572b98e123272348fd92693f1722f60a7f51
freeftr <jonghaparkdevelop@gmail.com>
2024-12-30 16:44:56 +0900

    feat: SpringSecurity 제거

--- src/main/java/com/ssafy/mozip/member/domain/Role.java
+++ src/main/java/com/ssafy/mozip/member/domain/Role.java
@@ -1,14 +1,6 @@
 package com.ssafy.mozip.member.domain;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
-    USER("ROLE_USER", "일반 사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
-
-    private final String key;
-    private final String title;
+    USER,
+    ADMIN;
 }
