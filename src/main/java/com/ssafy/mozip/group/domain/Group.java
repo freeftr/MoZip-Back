d1f3f87d6625e0b79d643d21b82362afc3068328
freeftr <jonghaparkdevelop@gmail.com>
2025-01-07 15:45:39 +0900

    feat: Group 도메인 정리

---
+++ src/main/java/com/ssafy/mozip/group/domain/Group.java
@@ -0,0 +1,25 @@
+package com.ssafy.mozip.group.domain;
+
+import com.ssafy.mozip.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Group extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long leaderId;
+
+
+}
---
+++ src/main/java/com/ssafy/mozip/group/domain/Participant.java
@@ -0,0 +1,24 @@
+package com.ssafy.mozip.group.domain;
+
+import com.ssafy.mozip.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Group group;
+
+}
